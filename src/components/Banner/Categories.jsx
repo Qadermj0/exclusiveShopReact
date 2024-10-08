187d66e5f67a1e7e206dc17d3a58eebe5a395a24
Qadermj0 <qadermajali56@gmail.com>
2024-10-08 13:57:55 +0300

    Fix-NotFound

--- src/components/Banner/Categories.jsx
+++ src/components/Banner/Categories.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./Banner.css";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
-  const data=[
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const data = [
     {
-      cateName:"Woman’s Fashion",
-       icon: "ri-arrow-right-s-line",
+      cateName: "Woman’s Fashion",
+      icon: "ri-arrow-right-s-line",
+      subCategories: ["Dresses", "Tops", "Shoes"],
     },
     {
-      cateName:"Men’s Fashion",
+      cateName: "Men’s Fashion",
       icon: "ri-arrow-right-s-line",
+      subCategories: ["Shirts", "Pants", "Jackets"],
     },{
       cateName:"Electronics",
      },{
@@ -26,23 +30,37 @@ const Categories = () => {
      },{
       cateName:"Health & Beauty",
      }
-  ]
-  return (
-    <> 
-      <div className='category'>
-        {data.map((value, index) => {
-          return (
-            <div className='box' key={index}>
+  ];
+  const toggleSubCategories = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
 
-              
-              <Link to='/shop'><span>{value.cateName}</span></Link>
-              <i className={value.icon}></i>
-            </div>
-          )
-        })}
+  return (
+    <>
+      <div className='category'>
+        {data.map((value, index) => (
+          <div className={`box ${activeIndex === index ? 'active' : ''}`} key={index}>
+            <Link to='/shop' onClick={() => toggleSubCategories(index)}>
+              <span>{value.cateName}</span>
+            </Link>
+            <i 
+              className={value.icon} 
+              onClick={() => toggleSubCategories(index)}
+            ></i>
+            {activeIndex === index && value.subCategories && (
+              <ul className='subcategories'>
+                {value.subCategories.map((sub, subIndex) => (
+                  <li key={subIndex}>
+                    <Link to='/shop'>{sub}</Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ))}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Categories;
