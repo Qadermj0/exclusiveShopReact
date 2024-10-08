4e3db3d0f071acbe0f38e78922e619c58ab264c1
Qadermj0 <qadermajali56@gmail.com>
2024-10-08 12:43:42 +0300

    fix delay

--- src/components/Banner/SliderBanner.jsx
+++ src/components/Banner/SliderBanner.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 import Sdata from "./Sdata";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
 const SilderBanner = () => {
   return (
     <>
@@ -12,43 +13,43 @@ const SilderBanner = () => {
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
+        modules={[Pagination, Autoplay]} // Include Autoplay module
+        autoplay={{
+          delay: 3000, // Move to the next slide every 2 seconds
+          disableOnInteraction: false, // Continue autoplay after user interaction
+        }}
         className="mySwiper"
       >
         {Sdata.map((value, index) => {
           return (
-            <>
-              <SwiperSlide>
-                <div className="homeSlide row mt-4" key={index}>
-                  <div className="col-md-6 w-50  ">
-                    <h1>
-                      <span>
-                        <img src={value.img} alt="" />
-                      </span>{" "}
-                      {value.title}
-                    </h1>
-                    <p>{value.desc}</p>
-                    <p>{value.desc2}</p>
-                    <div className="btn-box d-flex align-items-center mt-3 ">
-                      <div>
-                        <Link to="/shop">
-                          <span className="button">Shop Now </span>{" "}
-                          <div className="line"></div>
-
-                        </Link>
-                      </div>
-
-                      <div>
-                        <i class="ri-arrow-right-line"></i>
-                      </div>
+            <SwiperSlide key={index}>
+              <div className="homeSlide row mt-4">
+                <div className="col-md-6 w-50">
+                  <h1>
+                    <span>
+                      <img src={value.img} alt="" />
+                    </span>{" "}
+                    {value.title}
+                  </h1>
+                  <p>{value.desc}</p>
+                  <p>{value.desc2}</p>
+                  <div className="btn-box d-flex align-items-center mt-3">
+                    <div>
+                      <Link to="/shop">
+                        <span className="button">Shop Now</span>{" "}
+                        <div className="line"></div>
+                      </Link>
+                    </div>
+                    <div>
+                      <i className="ri-arrow-right-line"></i>
                     </div>
                   </div>
-                  <div className="col-md-6 photo w-50   ">
-                    <img src={value.cover} alt="" />
-                  </div>
                 </div>
-              </SwiperSlide>
-            </>
+                <div className="col-md-6 photo w-50">
+                  <img src={value.cover} alt="" />
+                </div>
+              </div>
+            </SwiperSlide>
           );
         })}
       </Swiper>
